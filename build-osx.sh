e3937638c2d7266a074a8491bb9bbe10749a4109
Kyle Partridge <kyle.partridge@sendgrid.com>
2015-01-26 09:43:49 -0800

    added lockfile

--- build-osx.sh
+++ build-osx.sh
@@ -1,7 +1,13 @@
+#!/bin/sh -e
+
 CFLAGS="-Wno-c++11-compat-deprecated-writable-strings -ISDL2-2.0.3/include"
 
 LDFLAGS="-LSDL2-2.0.3/lib -lSDL2"
 
+
+lockfile=build/build.lock
+touch $lockfile
 mkdir -p build
 clang++ game.cpp $CFLAGS $LDFLAGS -shared -undefined dynamic_lookup -o build/game.so
 clang++ sdl_platform.cpp $CFLAGS $LDFLAGS -o build/sdl_platform
+rm $lockfile
