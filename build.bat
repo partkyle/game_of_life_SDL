96d5935c00715f6b4ac8d593796e812848d3314f
partkyle <partkyle@gmail.com>
2024-04-03 13:51:01 -0700

    update build to work with SDL 2.30

--- build.bat
+++ build.bat
@@ -8,5 +8,5 @@ echo WAITING FOR PDB > game.dll.lock
 del game*.pdb
 cl -nologo  -Z7 -Gm- -GR- -EHa- -EHsc -Od -Oi -FC -fp:fast ..\game.cpp -Fmgame.map -LD -link -incremental:no -PDB:game_%random%.pdb -EXPORT:GameUpdateAndRender
 del game.dll.lock
-cl -nologo -Z7 -Gm- -GR- -EHa- -EHsc ..\sdl_platform.cpp -MD -Od -Oi -FC -fp:fast -I ..\vendor\SDL2-2.0.3\include -link ..\vendor\SDL2-2.0.3\lib\x64\SDL2main.lib ..\vendor\SDL2-2.0.3\lib\x64\SDL2.lib -SUBSYSTEM:WINDOWS
+cl -nologo -Z7 -Gm- -GR- -EHa- -EHsc ..\sdl_platform.cpp -MD -Od -Oi -FC -fp:fast -I ..\vendor\SDL2-2.30.2\include -link ..\vendor\SDL2-2.30.2\lib\x64\SDL2main.lib ..\vendor\SDL2-2.30.2\lib\x64\SDL2.lib shell32.lib -SUBSYSTEM:WINDOWS
 popd
